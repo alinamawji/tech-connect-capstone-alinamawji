2656e4133eaecf7834d3633497f85f020167ad78
foxaidan <aidanfox101@gmail.com>
2021-08-12 11:37:34 -0400

    Created cookbook controller

--- src/main/java/com/techelevator/controller/CookbookController.java
+++ src/main/java/com/techelevator/controller/CookbookController.java
@@ -1,9 +1,7 @@
 package com.techelevator.controller;
 
 
-import com.techelevator.model.JDBCCookbookDAO;
-import com.techelevator.model.JDBCRecipeDAO;
-import com.techelevator.model.User;
+import com.techelevator.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -12,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
 @Controller
 public class CookbookController {
 
@@ -19,17 +20,32 @@ public class CookbookController {
     private JDBCRecipeDAO recipeDAO;
     private JDBCCookbookDAO cookbookDAO;
 
-    @RequestMapping (path="/cookbook", method = RequestMethod.GET)
-    public String getCookbook() { return "cookbook";}
-
-    @RequestMapping (path="/cookbook", method = RequestMethod.POST) //only one GET and POST per page
-    public String displayMySavedRecipes(@RequestParam long user_id, ModelMap modelHolder){
-        modelHolder.put("savedRecipes", cookbookDAO.getRecipesFromMyCookbook(user_id));
-        return "cookbook";
+    @RequestMapping(path = "/cookbook", method = RequestMethod.GET)
+    public String getSavedRecipes(HttpSession session, ModelMap modelHolder) {
+        User user = new User();
+        if(session.getAttribute("user") != null) {
+            user = (User) session.getAttribute("user");
+            modelHolder.put("savedRecipes", cookbookDAO.getRecipesFromMyCookbook(user.getId()));
+            return "cookbook";
+        } else { return "private";}
     }
 
-//    @RequestMapping (path = "/recipeDetails")
+    @RequestMapping(path = "/recipeDetails", method = RequestMethod.GET)
+    public String displayRecipeDetails(@RequestParam Long recipe_id, ModelMap modelHolder, HttpSession session) {
+        Recipe recipe = recipeDAO.getRecipeByID(recipe_id);
+        modelHolder.put("recipe", recipe);
+        List<Ingredient> ingredients = recipeDAO.getRecipeIngredients(recipe_id);
+        modelHolder.put("ingredients", ingredients);
+        session.getAttribute("user");
 
+        return "recipeDetails";
+    }
+
+
+
+
+
+//    @RequestMapping (path = "/recipeDetails")
 
 
 }
